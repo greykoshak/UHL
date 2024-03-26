9137e5c71136789893bf6ffc4bc73c1373b60642
Dev Test <devtestdev.009@gmail.com>
2024-03-26 18:47:35 +0200

    change equipment db model

--- data_model_dbeaver/SQL.sql
+++ data_model_dbeaver/SQL.sql
@@ -53,8 +53,15 @@ WITH DATA;
 
 select * from mv_equipment
 
-select e.equipment_id, me.type_name, me.equip_name, ds.state_name, dp.person_name, dp.sex 
+select e.equipment_id, me.type_name, me.equip_name, ds.state_name, dp.person_name 
 from equipment e, mv_equipment me, dr_state ds, dr_person dp 
 where e.ln_equipment_id = me.ln_equipment_id 
 and e.dr_state_id = ds.dr_state_id 
 and e.dr_person_id = dp.dr_person_id 
+union all 
+select e.equipment_id, me.type_name, me.equip_name, ds.state_name, ds.state_name 
+from equipment e, mv_equipment me, dr_state ds 
+where e.ln_equipment_id = me.ln_equipment_id 
+and e.dr_state_id = ds.dr_state_id 
+and ds.dr_state_id = 1
+order by 1
